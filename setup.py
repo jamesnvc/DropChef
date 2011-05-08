fdce60cc8dc8ba3112c6d606046f1c017b93eb8b
James N. V. Cash <james.nvc@gmail.com>
2011-05-08 15:48:35 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+
+from distutils import core
+
+core.setup(name='DropChef',
+           version='0.1',
+           author='James Cash',
+           author_email='james.cash@occasionallycogent.com',
+           url='http://github.com/jamesnvc/dropchef',
+           packages=['dropchef'],
+           )
